1d0be7334f8aa9590f5ecaccd8fffde98af4bf41
PratibhaMastud <karandepratibha999@gmail.com>
2020-09-21 18:47:12 +0530

    Updates

---
+++ precache-manifest.e0717965d617037753c13aa42d738b20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654dff9a0423e5da305c5d026c565549",
+    "url": "/reactwebsiteportfolio/index.html"
+  },
+  {
+    "revision": "0673a808f52a870bb1d9",
+    "url": "/reactwebsiteportfolio/static/css/main.bc6bdf3a.chunk.css"
+  },
+  {
+    "revision": "24905281502fdea2f65b",
+    "url": "/reactwebsiteportfolio/static/js/2.8c421655.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/reactwebsiteportfolio/static/js/2.8c421655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0673a808f52a870bb1d9",
+    "url": "/reactwebsiteportfolio/static/js/main.fa67ad4f.chunk.js"
+  },
+  {
+    "revision": "42a65f0e47e66e157557",
+    "url": "/reactwebsiteportfolio/static/js/runtime-main.14b63561.js"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/reactwebsiteportfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/reactwebsiteportfolio/static/media/react-redux.a8b012a0.jpg"
+  }
+]);
